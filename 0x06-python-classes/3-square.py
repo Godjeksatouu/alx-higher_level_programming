5fbcf6d104bb05ceef7570200af3af703bd2c4ab
Godjeksatouu <godjeksatou@gmail.com>
2023-11-21 09:24:01 -0800

    task 2.3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    def __init__(self, size=0):
+        self.set_size(size)
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
