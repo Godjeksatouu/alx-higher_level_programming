732d0041045f3deb406366f29802a49bd451ff4d
Godjeksatouu <godjeksatou@gmail.com>
2023-11-21 09:32:49 -0800

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 class Square:
     """
-    creates a square object
+    Represents a square object.
     """
-    def __init__(self, size=0):
-        if(type(size) is not int):
-            raise TypeError("size must be an integer")
-        if(size < 0):
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    def __init__(self, side_length=0):
+        """
+        Initializes an instance of a square.
+
+        Args:
+            side_length (int): Length of the square's side.
+                It must be a positive integer.
+        """
+        if type(side_length) is not int:
+            raise TypeError("side_length must be an integer")
+        if side_length < 0:
+            raise ValueError("side_length must be >= 0")
+        self.__side_length = side_length
