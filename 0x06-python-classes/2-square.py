a6ab5c7ae1cfd644456ca28ebbbac4a225230b96
Godjeksatouu <godjeksatou@gmail.com>
2023-11-21 09:28:41 -0800

    task 2 update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""Sqaure module."""
-
-
 class Square:
+    """
+    creates a square object
+    """
     def __init__(self, size=0):
-        self.set_size(size)
-
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, size):
-        if not isinstance(size, int):
+        if(type(size) is not int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if(size < 0):
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
