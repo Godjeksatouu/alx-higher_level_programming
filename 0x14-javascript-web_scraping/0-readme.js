28eaa73d55b051915d6f024ac3c80a5d31991c1a
Godjeksatouu <godjeksatou@gmail.com>
2024-03-26 11:01:12 +0000

    fixe bugg

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+// Import the built-in Node.js 'fs' module.
 
-const filePath = process.argv[2];
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  // Use fs.readFile() to read the contents of a file specified as a command-line argument
+  // 'utf8' specifies the encoding of the file being read
 
-try {
-const content = fs.readFileSync(filePath, 'utf8');
-console.log(`The content of the file ${filePath} is:`);
-console.log(content);
-} catch (error) {
-console.error(`Error reading file ${filePath}:`, error);
-}
+  if (error) {
+    // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
+    console.error('Error reading the file:', error);
+
+  } else {
+    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
+    console.log(content);
+  }
+});
+
