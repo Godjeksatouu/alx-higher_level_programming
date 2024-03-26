44790dd463b6e6bc9b3aa79383e32667ba9e666a
Godjeksatouu <godjeksatou@gmail.com>
2024-03-26 10:51:12 +0000

    all

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-	console.log(error || content);
-});
+
+const filePath = process.argv[2];
+
+try {
+const content = fs.readFileSync(filePath, 'utf8');
+console.log(`The content of the file ${filePath} is:`);
+console.log(content);
+} catch (error) {
+console.error(`Error reading file ${filePath}:`, error);
+}
